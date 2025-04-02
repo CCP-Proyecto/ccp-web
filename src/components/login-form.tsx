c4294b892e39a3e7fe0aa02cb1feb28ae9278c71
Camilo Romero <ca.romerom1@uniandes.edu.co>
2025-04-01 22:49:15 -0500

    feat: include login page

---
+++ src/components/login-form.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { authClient } from "@/lib/auth-client";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    const { data, error } = await authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        body: {
+          app: "ccp-web",
+        },
+      },
+    );
+
+    if (error) {
+      console.log(error);
+    }
+
+    // redirect to dashboard
+    console.log(data);
+    console.log(data?.user);
+  };
+
+  const handleSignUp = async () => {
+    const { data, error } = await authClient.signUp.email({
+      email,
+      password,
+      name: "Test",
+      roles: ["admin"],
+    });
+
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(data);
+    console.log(data?.user);
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button className="w-full" onClick={handleLogin} type="button">
+                  Login
+                </Button>
+                <Button
+                  className="w-full"
+                  variant="secondary"
+                  onClick={handleSignUp}
+                  type="button"
+                >
+                  Sign up
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
