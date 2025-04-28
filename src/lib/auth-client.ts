e286b829587fae4e8f827e4e34a592ad12a47b39
Camilo Romero <ca.romerom1@uniandes.edu.co>
2025-04-27 23:21:58 -0500

    chore: update session url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,7 @@
-import { env } from "@/env";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signOut, signUp, useSession } = createAuthClient({
-  baseURL: `http://localhost:${process.env.PORT ?? 3000}`,
   plugins: [
     inferAdditionalFields({
       user: {
