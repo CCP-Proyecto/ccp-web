7162d49fae5108a9f00b82c3526cf28a89740150
Camilo Romero <ca.romerom1@uniandes.edu.co>
2025-05-11 22:31:47 -0500

    feat(statements): add query views

---
+++ src/app/salesmans/statements/view/[salesmanId]/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { api } from "@/trpc/react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchProductsPage() {
+  const [selectedStatement, setSelectedStatement] = useState<string>("");
+  const params = useParams<{ salesmanId: string }>();
+
+  const router = useRouter();
+  const { data: statements, isLoading } =
+    api.salesman.getSalesmanStatements.useQuery({
+      salesmanId: params.salesmanId,
+    });
+
+  const handleStatementSelection = (value: string) => {
+    setSelectedStatement(value);
+  };
+
+  const handleContinuar = () => {
+    if (selectedStatement) {
+      router.push(
+        `/salesmans/statements/view/${params.salesmanId}/${selectedStatement}`,
+      );
+    }
+  };
+
+  const handleVolver = () => {
+    router.back();
+  };
+
+  if (isLoading || !statements) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center px-4 py-8">
+      <div className="w-full max-w-md">
+        <h1 className="mb-12 font-normal text-3xl">
+          Seleccione el informe que quiere consultar
+        </h1>
+
+        <Select onValueChange={handleStatementSelection}>
+          <SelectTrigger className="h-14 w-full rounded-full border-gray-300">
+            <SelectValue placeholder="Selecciona un informe" />
+          </SelectTrigger>
+          <SelectContent>
+            {statements.map((salesman) => (
+              <SelectItem key={salesman.id} value={salesman.id}>
+                {salesman.id}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        {selectedStatement && (
+          <div className="mt-8 flex justify-center">
+            <Button onClick={handleContinuar} variant="primaryCCP">
+              Continuar
+            </Button>
+          </div>
+        )}
+
+        <div className="mt-8 flex justify-center">
+          <Button onClick={handleVolver} variant="primaryCCP">
+            Volver
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/server/api/routers/salesman.ts
+++ src/server/api/routers/salesman.ts
@@ -4,6 +4,42 @@ import { env } from "@/env";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
+interface Salesman {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+}
+
+interface Statement {
+  id: number;
+  description: string;
+}
+
+const getAllSalesmanStatements = async (
+  headers: Headers,
+  salesmanId: string,
+) => {
+  const res = await fetch(
+    `${env.API_MS}/api/statement?salespersonId=${salesmanId}`,
+    {
+      headers,
+    },
+  );
+
+  if (!res.ok) {
+    const { error } = await res.json();
+    if (res.status === 401) {
+      throw new TRPCError({ code: "UNAUTHORIZED", message: error });
+    }
+    throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: error });
+  }
+
+  const data: Statement[] = await res.json();
+
+  return data;
+};
+
 export const salesmanRouter = createTRPCRouter({
   createSalesman: protectedProcedure
     .input(
@@ -34,5 +70,71 @@ export const salesmanRouter = createTRPCRouter({
       }
 
       return res.json();
+    }),
+  getAllSalesmans: protectedProcedure.query(async ({ ctx }) => {
+    const headers = new Headers();
+    ctx.cookie && headers.set("Cookie", ctx.cookie);
+
+    const res = await fetch(`${env.API_MS}/api/salesperson`, {
+      headers,
+    });
+
+    if (!res.ok) {
+      const { error } = await res.json();
+      if (res.status === 401) {
+        throw new TRPCError({ code: "UNAUTHORIZED", message: error });
+      }
+      throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: error });
+    }
+
+    const data: Salesman[] = await res.json();
+
+    return data;
+  }),
+  getSalesmanStatements: protectedProcedure
+    .input(
+      z.object({
+        salesmanId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const headers = new Headers();
+      ctx.cookie && headers.set("Cookie", ctx.cookie);
+
+      const salesmanStatements = await getAllSalesmanStatements(
+        headers,
+        input.salesmanId,
+      );
+
+      return salesmanStatements;
+    }),
+  getSalesmanStatementById: protectedProcedure
+    .input(
+      z.object({
+        salesmanId: z.string(),
+        statementId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const headers = new Headers();
+      ctx.cookie && headers.set("Cookie", ctx.cookie);
+
+      const salesmanStatements = await getAllSalesmanStatements(
+        headers,
+        input.salesmanId,
+      );
+
+      const statement = salesmanStatements.find(
+        (s) => s.id === Number(input.statementId),
+      );
+
+      if (!statement) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `No se encontró la declaración con ID ${input.statementId}`,
+        });
+      }
+
+      return statement;
     }),
 });
